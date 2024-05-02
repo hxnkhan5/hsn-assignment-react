12bdde5c1304a6c352aee14750a189b5d28ae413
hxnkhan5 <hxnkhan5>
2024-05-02 13:22:36 +0000

    First Commit from Locofy Builder

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,314 @@
+import { FunctionComponent } from "react";
+import "./dashboard.css";
+
+const Dashboard: FunctionComponent = () => {
+  return (
+    <div className="dashboard-5">
+      <div className="my-portfolio">My portfolio</div>
+      <div className="frame-parent">
+        <div className="frame-group">
+          <div className="dashboard-wrapper">
+            <div className="dashboard">Dashboard</div>
+          </div>
+          <img className="frame-child" alt="" src="/line-1.svg" />
+        </div>
+        <div className="frame-container">
+          <div className="dashboard-wrapper">
+            <div className="mis-updates">{`MIS & Updates`}</div>
+          </div>
+          <img className="line-1-stroke" alt="" src="/line-1-stroke.svg" />
+        </div>
+        <div className="frame-group">
+          <div className="dashboard-wrapper">
+            <div className="dashboard">Insights</div>
+          </div>
+          <img className="frame-item" alt="" src="/line-11.svg" />
+        </div>
+      </div>
+      <div className="dashboard-5-child" />
+      <div className="frame-parent1">
+        <div className="frame-parent2">
+          <div className="frame-parent3">
+            <div className="investor-update-wrapper">
+              <div className="investor-update">Investor update</div>
+            </div>
+            <img className="more-vert-icon" alt="" src="/more-vert.svg" />
+          </div>
+          <div className="frame-parent4">
+            <img className="frame-inner" alt="" src="/frame-159825@2x.png" />
+            <div className="ace-green-recycling-parent">
+              <div className="ace-green-recycling">Ace Green Recycling</div>
+              <div className="battery-recycling-parent">
+                <div className="battery-recycling">Battery Recycling</div>
+                <div className="battery-recycling">|</div>
+                <div className="battery-recycling">Bengaluru</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="line-div" />
+        <div className="frame-child1" />
+        <div className="lorem-ipsum-dolor-container">
+          <p className="lorem-ipsum-dolor">
+            Lorem ipsum dolor sit amet consectetur. Nulla urna quis ipsum lectus
+            adipiscing. Egestas et mi tortor pretium convallis. Vel eu tincidunt
+            commodo quisque nunc sit ac vulputate orci. Tincidunt vel vel
+            ultricies enim integer ac blandit. Vestibulum maecenas cursus
+            feugiat vestibulum congue. Turpis sit enim tincidunt blandit.
+            Pellentesque porttitor scelerisque ut risus facilisis dignissim in
+            amet hendrerit. Porttitor risus netus et in. Lacus tincidunt
+            dignissim risus quam diam vivamus proin volutpat.
+          </p>
+          <p className="lorem-ipsum-dolor">Massa ut et sed mattis tristique.</p>
+          <p className="lorem-ipsum-dolor">
+            Pulvinar vehicula tellus ut egestas leo sed. Malesuada auctor
+            vestibulum luctus maecenas maecenas aliquam tellus viverra.
+          </p>
+        </div>
+        <div className="pulvinar-vehicula-tellus1">
+          Pulvinar vehicula tellus ut egestas leo sed. Malesuada auctor
+          vestibulum luctus maecenas maecenas aliquam tellus viverra.
+        </div>
+        <img
+          className="unsplashshr-xn8s8qu-icon"
+          alt=""
+          src="/unsplashshr-xn8s8qu@2x.png"
+        />
+        <div className="read-more">READ MORE</div>
+        <div className="days-ago">4 days ago</div>
+      </div>
+      <div className="frame-parent5">
+        <div className="frame-parent2">
+          <div className="frame-parent3">
+            <div className="investor-update-wrapper">
+              <div className="investor-update">Feature launch</div>
+            </div>
+            <img className="more-vert-icon" alt="" src="/more-vert.svg" />
+          </div>
+          <div className="frame-parent4">
+            <img className="frame-inner" alt="" src="/frame-159825@2x.png" />
+            <div className="ace-green-recycling-parent">
+              <div className="ace-green-recycling">Ace Green Recycling</div>
+              <div className="battery-recycling-parent">
+                <div className="battery-recycling">Battery Recycling</div>
+                <div className="battery-recycling">|</div>
+                <div className="battery-recycling">Bengaluru</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="line-div" />
+        <div className="frame-child3" />
+        <div className="lorem-ipsum-dolor-container">
+          <p className="lorem-ipsum-dolor">
+            Lorem ipsum dolor sit amet consectetur. Nulla urna quis ipsum lectus
+            adipiscing. Egestas et mi tortor pretium convallis. Vel eu tincidunt
+            commodo quisque nunc sit ac vulputate orci. Tincidunt vel vel
+            ultricies enim integer ac blandit. Vestibulum maecenas cursus
+            feugiat vestibulum congue. Turpis sit enim tincidunt blandit.
+            Pellentesque porttitor scelerisque ut risus facilisis dignissim in
+            amet hendrerit. Porttitor risus netus et in. Lacus tincidunt
+            dignissim risus quam diam vivamus proin volutpat.
+          </p>
+          <p className="lorem-ipsum-dolor">Massa ut et sed mattis tristique.</p>
+          <p className="lorem-ipsum-dolor">
+            Pulvinar vehicula tellus ut egestas leo sed. Malesuada auctor
+            vestibulum luctus maecenas maecenas aliquam tellus viverra.
+          </p>
+        </div>
+        <div className="read-more1">READ MORE</div>
+        <div className="div2">15-03-2024</div>
+      </div>
+      <div className="frame-parent9">
+        <div className="frame-parent10">
+          <div className="frame-parent11">
+            <div className="frame-parent12">
+              <div className="investor-update-wrapper">
+                <div className="investor-update">Fund investment update</div>
+              </div>
+              <div className="ellipse-parent">
+                <div className="ellipse-div" />
+                <div className="live-deal">Live deal</div>
+              </div>
+            </div>
+            <img className="more-vert-icon" alt="" src="/more-vert.svg" />
+          </div>
+          <div className="frame-parent4">
+            <img className="frame-inner" alt="" src="/frame-159825@2x.png" />
+            <div className="ace-green-recycling-parent">
+              <div className="ace-green-recycling">Ace Green Recycling</div>
+              <div className="battery-recycling-parent">
+                <div className="battery-recycling">Battery Recycling</div>
+                <div className="battery-recycling">|</div>
+                <div className="battery-recycling">Bengaluru</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="line-div" />
+        <div className="frame-child6" />
+        <div className="lorem-ipsum-dolor-container">{`Lorem ipsum dolor sit amet consectetur. Nulla urna quis ipsum lectus adipiscing. Egestas et mi tortor pretium convallis. Vel eu tincidunt commodo quisque nunc sit ac vulputate orci. Tincidunt vel vel ultricies enim integer ac blandit. Vestibulum maecenas cursus feugiat vestibulum congue. Turpis sit enim tincidunt blandit. Pellentesque porttitor scelerisque ut risus facilisis dignissim in amet hendrerit. Porttitor risus netus et in. `}</div>
+        <div className="div4">15-03-2024</div>
+        <img
+          className="unsplashwwqrpsnbpq4-icon"
+          alt=""
+          src="/unsplashwwqrpsnbpq4@2x.png"
+        />
+        <div className="buttons">
+          <div className="button-text">View deal</div>
+        </div>
+      </div>
+      <div className="activity-feed">Activity feed</div>
+      <div className="frame-parent14">
+        <div className="frame-wrapper">
+          <div className="search-parent">
+            <img className="more-vert-icon" alt="" src="/search.svg" />
+            <div className="search-by-company">Search by company name...</div>
+          </div>
+        </div>
+        <div className="filter-list-parent">
+          <img className="more-vert-icon" alt="" src="/filter-list.svg" />
+          <div className="filter">Filter</div>
+        </div>
+      </div>
+      <div className="frame-parent15">
+        <div className="instance-parent">
+          <div className="component-12-parent">
+            <img className="more-vert-icon" alt="" src="/component-12.svg" />
+            <div className="portfolio">Home</div>
+            <img
+              className="arrow-back-ios-new-icon"
+              alt=""
+              src="/arrow-back-ios-new@2x.png"
+            />
+            <img className="expand-more-icon" alt="" src="/expand-more.svg" />
+          </div>
+          <div className="component-12-group">
+            <img className="more-vert-icon" alt="" src="/component-121.svg" />
+            <div className="portfolio1">Portfolio</div>
+            <img
+              className="arrow-back-ios-new-icon"
+              alt=""
+              src="/arrow-back-ios-new@2x.png"
+            />
+            <img className="expand-more-icon" alt="" src="/expand-more1.svg" />
+          </div>
+          <div className="component-12-container">
+            <img className="more-vert-icon" alt="" src="/component-122.svg" />
+            <div className="portfolio">Invest</div>
+            <img
+              className="arrow-back-ios-new-icon"
+              alt=""
+              src="/arrow-back-ios-new@2x.png"
+            />
+            <img className="more-vert-icon" alt="" src="/expand-more.svg" />
+          </div>
+          <div className="component-12-container">
+            <img className="more-vert-icon" alt="" src="/component-123.svg" />
+            <div className="portfolio">Syndicates</div>
+            <img
+              className="arrow-back-ios-new-icon"
+              alt=""
+              src="/arrow-back-ios-new@2x.png"
+            />
+            <img className="expand-more-icon" alt="" src="/expand-more.svg" />
+          </div>
+          <div className="component-12-container">
+            <img className="more-vert-icon" alt="" src="/component-124.svg" />
+            <div className="portfolio">Ledger</div>
+            <img
+              className="arrow-back-ios-new-icon"
+              alt=""
+              src="/arrow-back-ios-new@2x.png"
+            />
+            <img className="expand-more-icon" alt="" src="/expand-more.svg" />
+          </div>
+          <div className="component-12-container">
+            <img className="more-vert-icon" alt="" src="/component-125.svg" />
+            <div className="portfolio">Messages</div>
+            <img
+              className="arrow-back-ios-new-icon"
+              alt=""
+              src="/arrow-back-ios-new@2x.png"
+            />
+            <img className="expand-more-icon" alt="" src="/expand-more.svg" />
+          </div>
+          <div className="component-12-container">
+            <img className="more-vert-icon" alt="" src="/component-126.svg" />
+            <div className="portfolio">Events</div>
+            <img
+              className="arrow-back-ios-new-icon"
+              alt=""
+              src="/arrow-back-ios-new@2x.png"
+            />
+            <img className="expand-more-icon" alt="" src="/expand-more.svg" />
+          </div>
+          <div className="component-12-container">
+            <img className="more-vert-icon" alt="" src="/component-127.svg" />
+            <div className="portfolio">{`News & Blogs`}</div>
+            <img
+              className="arrow-back-ios-new-icon"
+              alt=""
+              src="/arrow-back-ios-new@2x.png"
+            />
+            <img className="expand-more-icon" alt="" src="/expand-more.svg" />
+          </div>
+          <div className="component-12-container">
+            <img className="more-vert-icon" alt="" src="/component-128.svg" />
+            <div className="portfolio">{`Help & Support`}</div>
+            <img
+              className="arrow-back-ios-new-icon"
+              alt=""
+              src="/arrow-back-ios-new@2x.png"
+            />
+            <img className="expand-more-icon" alt="" src="/expand-more.svg" />
+          </div>
+        </div>
+        <div className="group-parent">
+          <img className="group-child" alt="" src="/group-1@2x.png" />
+          <img
+            className="climate-angels-icon"
+            alt=""
+            src="/climate-angels.svg"
+          />
+        </div>
+      </div>
+      <div className="dashboard-5-inner">
+        <div className="frame-parent16">
+          <div className="frame-wrapper1">
+            <div className="search-group">
+              <img className="more-vert-icon" alt="" src="/search.svg" />
+              <div className="battery-recycling">Search by company name...</div>
+            </div>
+          </div>
+          <div className="frame-parent17">
+            <div className="notifications-parent">
+              <img
+                className="notifications-icon"
+                alt=""
+                src="/notifications.svg"
+              />
+              <div className="wrapper">
+                <div className="div5">4</div>
+              </div>
+            </div>
+            <div className="frame-parent18">
+              <img className="frame-child7" alt="" src="/frame-159360@2x.png" />
+              <div className="shailesh-vickram-parent">
+                <div className="mis-updates">Shailesh Vickram</div>
+                <div className="investor">Investor</div>
+              </div>
+              <img
+                className="more-vert-icon"
+                alt=""
+                src="/expand-circle-down.svg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
